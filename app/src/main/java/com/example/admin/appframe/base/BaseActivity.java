f9b52d430ccdab008062b2572e19b5e153d638f6
wenquan <2944660600@qq.com>
2017-04-24 09:31:42 +0800

    建立项目，android快速开发框架

---
+++ app/src/main/java/com/example/admin/appframe/base/BaseActivity.java
@@ -0,0 +1,11 @@
+package com.example.admin.appframe.base;
+
+import android.app.Activity;
+
+/**
+ * Created by admin on 2017/4/24.
+ */
+
+public class BaseActivity extends Activity {
+
+}
---
+++ app/src/main/java/com/example/admin/appframe/base/BaseView.java
@@ -0,0 +1,8 @@
+package com.example.admin.appframe.base;
+
+/**
+ * Created by admin on 2017/4/24.
+ */
+
+public class BaseView {
+}
