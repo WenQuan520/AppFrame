cfd97d5dd73f05bb6cf93ce88318cb01619cde01
wenquan <2944660600@qq.com>
2017-04-24 09:36:39 +0800

    建立项目，android快速开发框架

--- app/src/main/java/com/example/admin/appframe/base/BaseActivity.java
+++ app/src/main/java/com/example/admin/appframe/base/BaseActivity.java
@@ -1,11 +1,26 @@
 package com.example.admin.appframe.base;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
 
 /**
  * Created by admin on 2017/4/24.
  */
 
-public class BaseActivity extends Activity {
+public abstract class BaseActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView();
+        initView();
+        initListener();
+        getData();
+    }
+
+    public abstract void setContentView();
+    public abstract void initView();
+    public abstract void initListener();
+    public abstract void getData();
 
 }
